7f4f81931ce0da3ba5666dafee80759ce713d360
bronnem1 <64006962+bronnem1@users.noreply.github.com>
2020-06-03 15:18:15 +0200

    Update Verkauf1Client.js

--- js/Verkauf1Client.js
+++ js/Verkauf1Client.js
@@ -1,11 +1,7 @@
 function Input1()
 {
   window.input1=document.getElementById("userInput").value;
-<<<<<<< HEAD
   //document.getElementById("userInput").value="0";
-=======
   document.getElementById("userInput").value="0";
   alert("Das Produkt wurde erfolgreich zum Warenkorb hinzugefügt !");
-  
->>>>>>> 4c362a9c2a2129d61cfa9f89620e5a5de1ff80f3
 }
