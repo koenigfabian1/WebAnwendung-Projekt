8d83876bd1e176e7ad66cc6d28ab66e9c1f79ad5
koenigfabian1 <63997678+koenigfabian1@users.noreply.github.com>
2020-06-05 15:51:27 +0200

    X

--- Backend/dao/personDao.js
+++ Backend/dao/personDao.js
@@ -18,14 +18,14 @@ class PersonDao {
         var statement = this._conn.prepare(sql);
         var result = statement.get(id);
 
-        if (helper.isUndefined(result)) 
+        if (helper.isUndefined(result))
             throw new Error("No Record found by id=" + id);
 
         result = helper.objectKeysToLower(result);
 
-        if (result.anrede == 0) 
+        if (result.anrede == 0)
             result.anrede = "Herr";
-        else 
+        else
             result.anrede = "Frau";
 
         result.geburtstag = helper.formatToGermanDate(helper.parseSQLDateTimeString(result.geburtstag));
@@ -44,19 +44,19 @@ class PersonDao {
         var statement = this._conn.prepare(sql);
         var result = statement.all();
 
-        if (helper.isArrayEmpty(result)) 
+        if (helper.isArrayEmpty(result))
             return [];
-        
+
         result = helper.arrayObjectKeysToLower(result);
 
         for (var i = 0; i < result.length; i++) {
-            if (result[i].anrede == 0) 
+            if (result[i].anrede == 0)
                 result[i].anrede = "Herr";
-            else 
+            else
                 result[i].anrede = "Frau";
 
             result[i].geburtstag = helper.formatToGermanDate(helper.parseSQLDateTimeString(result[i].geburtstag));
-            
+
             for (var element of addresses) {
                 if (element.id == result[i].adresseid) {
                     result[i].adresse = element;
@@ -74,32 +74,32 @@ class PersonDao {
         var statement = this._conn.prepare(sql);
         var result = statement.get(id);
 
-        if (result.cnt == 1) 
+        if (result.cnt == 1)
             return true;
 
         return false;
     }
 
-    create(anrede = "Herr", vorname = "", nachname = "", adresseid = 1, telefonnummer = "", email = "", geburtstag = null) {
-        var sql = "INSERT INTO Person (Anrede,Vorname,Nachname,AdresseID,Telefonnummer,Email,Geburtstag) VALUES (?,?,?,?,?,?,?)";
+    create(anrede = "Herr", vorname = "", nachname = "", adresseid = 1) {
+        var sql = "INSERT INTO Person (Anrede,Vorname,Nachname,AdresseID) VALUES (?,?,?,?)";
         var statement = this._conn.prepare(sql);
         var params = [(helper.strStartsWith(anrede, "He") ? 0 : 1), vorname, nachname, adresseid, telefonnummer, email, (helper.isNull(geburtstag) ? null : helper.formatToSQLDate(geburtstag))];
         var result = statement.run(params);
 
-        if (result.changes != 1) 
+        if (result.changes != 1)
             throw new Error("Could not insert new Record. Data: " + params);
 
         var newObj = this.loadById(result.lastInsertRowid);
         return newObj;
     }
 
-    update(id, anrede = "Herr", vorname = "", nachname = "", adresseid = 1, telefonnummer = "", email = "", geburtstag = null) {
-        var sql = "UPDATE Person SET Anrede=?,Vorname=?,Nachname=?,AdresseID=?,Telefonnummer=?,Email=?,Geburtstag=? WHERE ID=?";
+    update(id, anrede = "Herr", vorname = "", nachname = "", adresseid = 1) {
+        var sql = "UPDATE Person SET Anrede=?,Vorname=?,Nachname=?,AdresseID=? WHERE ID=?";
         var statement = this._conn.prepare(sql);
         var params = [(helper.strStartsWith(anrede, "He") ? 0 : 1), vorname, nachname, adresseid, telefonnummer, email, (helper.isNull(geburtstag) ? null : helper.formatToSQLDate(geburtstag)), id];
         var result = statement.run(params);
 
-        if (result.changes != 1) 
+        if (result.changes != 1)
             throw new Error("Could not update existing Record. Data: " + params);
 
         var updatedObj = this.loadById(id);
@@ -112,7 +112,7 @@ class PersonDao {
             var statement = this._conn.prepare(sql);
             var result = statement.run(id);
 
-            if (result.changes != 1) 
+            if (result.changes != 1)
                 throw new Error("Could not delete Record by id=" + id);
 
             return true;
