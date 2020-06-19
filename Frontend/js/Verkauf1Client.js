722ef0d45acba076bbe00015498d4764a92eae4c
koenigfabian1 <63997678+koenigfabian1@users.noreply.github.com>
2020-06-19 17:50:33 +0200

    X

--- Frontend/js/Verkauf1Client.js
+++
@@ -1,9 +0,0 @@
-function NotAvail()
-{
-  if (document.getElementById("userInput").value === '') { //If the client entered something else then a number
-    alert('Unzulässige Eingabe!');
-  }else{
-      alert("-Das Produkt wurde nicht zum Warenkorb hinzugefügt! - Produkt nicht auf Lager.");
-  }
-  window.location.reload(true);
-}
