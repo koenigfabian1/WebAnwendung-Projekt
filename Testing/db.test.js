0bc2c5218cb60204ace6d8773494119f012c4e7c
bronnem1 <64006962+bronnem1@users.noreply.github.com>
2020-07-06 15:54:17 +0200

    Update db.test.js

--- Testing/db.test.js
+++ Testing/db.test.js
@@ -13,18 +13,3 @@ module.exports=ProduktDao;
 
 
 
-test('Test ob die Connection vorhanden ist', () => {
-    expect(dao.getConnection()).toBeTruthy();
-  });
-
-test('Bieten wir in unserem Shop das Produkt ALCLEAR Waschhandschuh an?', () => {
-    expect(dao.loadById(1).name).toEqual("ALCLEAR Waschhandschuh");
-  });
-
-test('Ist der Nettopreis der Lackknete korrekt', () => {
-    expect(dao.loadAllByCategoryId(1)[1].nettopreis).toEqual(14.277);
-  });
-
-test('Gibt es bei der Tabelle Mehrwertsteuer den Eintrag mit der ID 1?', () => {
-    expect(dao2.exists(1)).toBeTruthy();
-  });
