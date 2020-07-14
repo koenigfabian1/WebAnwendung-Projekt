7845e07cdcc5d25c8160550c22ef7dcca28404c5
2muchJohn <51917955+2muchJohn@users.noreply.github.com>
2020-07-14 15:23:57 +0200

    Update db.test.js

--- Testing/db.test.js
+++ Testing/db.test.js
@@ -3,7 +3,7 @@ const dbOptions = {verbose: console.log};
 const dbFile = "../Backend/db/db.sqlite";
 const dbConnection = new Database(dbFile, dbOptions);
 const ProduktDao = require("../Backend/dao/produktDao");
-const MehrwertsteuerDao = require("../Backend/dao/mehrwertsteuerdao");
+const MehrwertsteuerDao = require("../Backend/dao/mehrwertsteuerDao");
 
 var dao=new ProduktDao(dbConnection);
 var dao2=new MehrwertsteuerDao(dbConnection);
